235b142b8d28a1011961d1ca927bdbce6b3c2e2b
Amen0920 <luj0605@gmail.com>
2025-06-19 00:53:23 +0900

    chore: add commitlint config, husky for commit message validation

---
+++ commitlint.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      ['feat', 'fix', 'docs', 'test', 'chore', 'refactor'],
+    ],
+    'type-case': [2, 'always', 'lower-case'],
+    'type-empty': [2, 'never'],
+    'scope-empty': [2, 'always'],
+    'subject-empty': [2, 'never'],
+    'subject-case': [0],
+  },
+};
